cb13f86b4f75ab800b08854cf4fa6364a28dff4f
SwordOfLink <theswordoflink@live.com>
2018-11-14 19:38:42 +0100

    Dernière ligne droit

---
+++ source/animation.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Animation
+{
+    public static string[,] TypoJAB = new string[,] 
+    {
+        {" ███ ","█████","████ "},
+        {"  █  ","█   █","█   █"},
+        {"  █  ","█████","████ "},
+        {"█ █  ","█   █","█   █"},
+        {" ██  ","█   █","████ "}
+    };
+    public static string[,] TypoTour = new string[,]
+    {
+        {"███","███","█ █","███"},
+        {" █ ","█ █","█ █","█  "},
+        {" █ ","███","███","█  " }
+    };
+    public static string[,] TypoMiniDe = new string[,]
+    {
+        {"   ","█  ","█  ","█ █","█ █","█ █" },
+        {" █ ","   "," █ ","   "," █ ","█ █" },
+        {"   ","  █","  █","█ █","█ █","█ █" }
+    };
+    private static int tailletitre = 156;
+    public static int TailleTitre
+    {
+        get
+        {
+            return tailletitre;
+        }
+    }
+    public static void titre(ConsoleColor[] couleur)
+    {
+        string[,] ligne = new string[,]
+        {
+            {"  ___  ____   "  ,"     _____    "," ____  _____  "  ,"    ______    " ,"  ________    " ,"     _____    ","     ______   "  ,"  _________   " },
+            {" |_  ||_  _|  "  ,"    |_   _|   ","|_   \\|_   _| " ,"  .' ___  |   " ," |_   ___ `.  " ,"    |_   _|   ","   .' ___  |  "  ," |_   ___  |  " },
+            {"   | |_/ /    "  ,"      | |     ","  |   \\ | |   " ," / .'   \\_|   ","   | |   `. \\ ","      | |     ","  / .'   \\_|  " ,"   | |_  \\_|  " },
+            {"   |  __'.    "  ,"      | |     ","  | |\\ \\| |   "," | |    ____  " ,"   | |    | | " ,"      | |     ","  | |         "  ,"   |  _|  _   " },
+            {"  _| |  \\ \\_  ","     _| |_    "," _| |_\\   |_  " ," \\ `.___]  _| ","  _| |___.' / " ,"     _| |_    ","  \\ `.___.'\\  ","  _| |___/ |  "},
+            {" |____||____| "  ,"    |_____|   ","|_____|\\____| " ,"  `._____.'   " ," |________.'  " ,"    |_____|   ","   `._____.'  "  ," |_________|  " },
+            {"              ","              ","              ","              ","              ","              ","              ","              "}
+        };
+
+        Console.WriteLine(" .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .----------------.  .----------------. ");
+        Console.WriteLine("| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |");
+        
+        //Display des milieus
+        for (int i = 0; i < ligne.GetLength(0); i++)
+        {
+            for (int j = 0; j < ligne.GetLength(1); j++)
+            {
+                Console.Write("| |");
+                if (j % 2 == 0)
+                    Console.ForegroundColor = couleur[0];
+                else
+                    Console.ForegroundColor = couleur[1];
+                Console.Write(ligne[i, j]);
+                Console.ResetColor();
+                Console.Write("| |");
+            }
+            Console.WriteLine("");
+        }
+
+        Console.WriteLine("| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |");
+        Console.WriteLine(" '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ");
+
+    }
+    public static void AnimationTitre(ConsoleColor[] couleur, int temps, int vitesse)
+    {
+        Random rng = new Random();
+        ConsoleColor[] couleurinverse = new ConsoleColor[] { couleur[1], couleur[0] };
+        for (int i = 0; i < temps; i++) {
+            Console.Clear();
+            if (i%2 == 0)
+            {
+                titre(couleur);
+            }
+            else
+            {
+                titre(couleurinverse);
+            }
+            System.Threading.Thread.Sleep(vitesse);
+        }
+    }
+    public static void AffDe(int de)
+    {
+        try
+        {
+            string[,] des = new string[,]
+            {
+                { "█████████","█       █","█       █","█       █","█   █   █","█       █","█       █","█       █","█████████" },
+                { "█████████","█       █","█ █     █","█       █","█       █","█       █","█     █ █","█       █","█████████" },
+                { "█████████","█       █","█ █     █","█       █","█   █   █","█       █","█     █ █","█       █","█████████" },
+                { "█████████","█       █","█ █   █ █","█       █","█       █","█       █","█ █   █ █","█       █","█████████" },
+                { "█████████","█       █","█ █   █ █","█       █","█   █   █","█       █","█ █   █ █","█       █","█████████" },
+                { "█████████","█       █","█ █   █ █","█       █","█ █   █ █","█       █","█ █   █ █","█       █","█████████" }
+            };
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write(des[de - 1, i]);
+                Console.SetCursorPosition(Console.CursorLeft - 9, Console.CursorTop + 1);
+            }
+            Console.WriteLine("");
+        }
+        catch(IndexOutOfRangeException)
+        {
+            Console.Write("Erreur index AffDe()");
+        }
+    }
+    public static void AnimationDe(int de)
+    {
+        Random rng = new Random();
+        int row = Console.CursorTop, col = Console.CursorLeft;
+        for (int i = 0; i < rng.Next(3, 5); i++)
+        {
+            AffDe(rng.Next(1, 6));
+            Console.SetCursorPosition(col, row);
+            System.Threading.Thread.Sleep(150);
+        }
+        AffDe(de);
+    }
+    public static void LigneDeDes(List<int> suitede) //Affiche une suite de petit dé pour le jeu 3
+    {
+        string ligne;
+        int j;
+        try
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                ligne = " ";
+                if (i == 1) Console.ForegroundColor = ConsoleColor.DarkGray;
+                else Console.ResetColor();
+                for (j = 0; j < suitede.Count; j++)
+                {
+                    ligne = ligne + TypoMiniDe[i, suitede[j] - 1] + " ";
+                }
+                Console.WriteLine(ligne);
+            }
+        }
+        catch(IndexOutOfRangeException)
+        {
+            Console.Write("Erreur index");
+        }
+    }
+    public static void EnteteTour(int JAouJB, string nom) //JAouJB == true tour du JA sinon tour du JB
+    {
+        int i, j;
+        string affichage = "";
+        for(i = 0; i < 5; i++)
+        {
+            for(j = 0; j<4; j++)
+            {
+                if(i == 0 || i == 1)
+                {
+                    affichage = affichage + "    ";
+                }
+                else
+                {
+                    affichage = affichage + TypoTour[i - 2, j] + " ";
+                }
+            }
+            for(j = 0; j<2;j++)
+            {
+                affichage = affichage + TypoJAB[i, j == 0 ? j : (JAouJB + 1)] + " ";
+            }
+            affichage = affichage + "\n";
+        }
+        affichage = affichage + "C'est au joueur " + nom + " de jouer !";
+        Console.WriteLine(affichage);
+    }
+    public static void ClearLigne(bool cond = true)
+    {
+        int currentLineCursor = Console.CursorTop;
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write(new string(' ', Console.WindowWidth - 1));
+        if(cond)
+            Console.SetCursorPosition(0, currentLineCursor);
+    }
+    public static void ClearResteEcran()
+    {
+        int currentLineCursor = Console.CursorTop;
+        for (int i = 0; i < Console.WindowHeight - currentLineCursor -1; i++)
+        {
+            ClearLigne(false);
+            Console.SetCursorPosition(0, Console.CursorTop + 1);
+        }
+    }
+    public static void AlaLigne()
+    {
+        Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
+    }
+}
---
+++ source/joueur.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TableJoueur
+{
+    //Champs
+    public List<Joueur> Table;
+    public Random rng;
+
+    //Constructeur
+    public TableJoueur(int nbJoueur)
+    {
+        int charac = 65; //65 pour le A necessitera une verification pour modifié les noms au dela de Z 
+        char c;
+        Table = new List<Joueur> { };
+        for(int i = 0; i < nbJoueur;i++)
+        {
+            c = Convert.ToChar(charac);
+            Table.Add(new Joueur(c.ToString()));
+            System.Threading.Thread.Sleep(5); //Permet d'avoir deux seeds différent d'aléatoire
+            charac++;
+        }
+        rng = new Random();
+        Tour = 0;
+    }
+
+    //Proprieté
+    public int Tour { get; private set; }
+    public int NbJoueur //Permet de raccoucir l'appel du nombre de joueur
+    {
+        get { return Table.Count; }
+    }
+    public Joueur Actuel //permet de simplifier l'appel du joueur a qui c'est le tour
+    {
+        get { return Table[Tour]; }
+    }
+
+    //Methode
+    public void QuiCommence() //Decide de qui commence le jeu; Ce base sur des taux de chance qui change en fonction de si le joueur a commencé avant et/ou si il perd
+    {
+        int totalChance = AddChance();
+        int palier = 0;
+        int chancePalier = Table[0].ChanceStart;
+        int jet = rng.Next(totalChance + 1);
+        while(jet > chancePalier)
+        {
+            palier++;
+            chancePalier += Table[0].ChanceStart;
+        }
+        if(Table[palier].Acommencer)
+        {
+            Table[palier].ChanceStart -= 10;
+        }
+        else
+        {
+            Table[palier].ChanceStart = 50;
+            Commence(palier);
+        }
+        Tour = palier;
+    }
+
+    private int AddChance()
+    {
+        int Total = 0;
+        for(int i = 0; i < Table.Count; i++)
+        {
+            Total += Table[i].ChanceStart;
+        }
+        return Total;
+    }
+
+    public void TourSuivant()
+    {
+        ++Tour;
+        if (Tour >= Table.Count) Tour = 0;
+    }
+
+    private void Commence(int index) //Permet de mettre a false toute les personne que ne commence pas et a true ce qui commence
+    {
+        for(int i = 0;i < Table.Count;i++)
+        {
+            if (i == index) Table[i].Acommencer = true;
+            else Table[i].Acommencer = false;
+        }
+    }
+
+    public bool TesteLimite(int limite)
+    {
+        for(int i = 0; i < NbJoueur; i++)
+        {
+            if (Table[i].Point >= limite) return true;
+        }
+        return false;
+    }
+
+    public Joueur QuiAGagner(int pts)
+    {
+        for(int i = 0; i < NbJoueur; i++)
+        {
+            if (Table[i].Point >= pts) return Table[i];
+        }
+        return Table[0];
+    }
+}
+public class Joueur
+{
+    //Champs
+    private Random _alea;
+    private int _chanceStart;
+    private string _nom;
+
+    //Constructeur
+    public Joueur(string nom) : this(nom, false) { }
+
+    public Joueur(string nom, bool bot)
+    {
+        _nom = nom;
+        Bot = bot;
+        StatD = new int[] { 0, 0, 0, 0, 0, 0 };
+        Nbvic = 0;
+        Point = 0;
+        ChanceStart = 50;
+        DernierDe = 0;
+        _alea = new Random(Guid.NewGuid().GetHashCode()); //une autre methode pour avoir de l'aléatoire permet de généré vraiment deux seed différente.
+    }
+
+    //Propriété
+    public int Point { get; set; }
+    public int DernierDe { get; private set; }
+    public int Nbvic { get; set; }
+    public int[] StatD { get; set; }
+    public string Nom
+    {
+        get
+        {
+            if (Bot) return "[bot] " + _nom;
+            else return _nom;
+        }
+        set { _nom = value; }
+    }
+    public bool Bot { get; set; }
+    public bool Acommencer { get; set; }
+    public int ChanceStart
+    {
+        get { return _chanceStart; }
+        set
+        {
+            if(value < 0) _chanceStart = 0;
+            else _chanceStart = value;
+        }
+    }
+
+
+    //Methode
+    public int JetDeDé()
+    {
+        int jet = _alea.Next(1, 7);
+        DernierDe = jet;
+        return jet;
+    }
+
+    public bool SetBot() //pour simplier l'OptBot
+    {
+        Bot = !Bot;
+        return Bot;
+    }
+
+}
